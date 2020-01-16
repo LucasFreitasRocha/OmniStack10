8bca3e3a08c1856c857156ae95e5ebe370855afb
Lucas Freitas <lucas.frietas@sigcertificadora.com.br>
2020-01-15 23:10:03 -0300

    dia 2 estilização

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,85 @@
-import React, {useState} from 'react';
-
-import Head from './Head'
+import React from 'react';
+import './index.css';
+import './app.css';
+import './sidebar.css';
+import './main.css'
 function App() {
-  const [counter,setCounter] = useState(0);
-  function incrementCounter(){
-    setCounter(counter +1);
-  }
+  
   return (
-    <>
-      <Head title="Hello world component!" />
-      <h1>Contador: {counter} </h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latidude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+        <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/33586465?v=4" alt="Lucas Rocha"/>
+              <div className="user-info">
+                <strong>Lucas Rocha</strong>
+                <span>php, ruby, js</span>
+              </div>
+            </header>
+            <p>Estudante de sistemas de informação no IFF campos campus centro.</p>
+            <a href="https://github.com/LucasFreitasRocha">Acessar perfil github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/33586465?v=4" alt="Lucas Rocha"/>
+              <div className="user-info">
+                <strong>Lucas Rocha</strong>
+                <span>php, ruby, js</span>
+              </div>
+            </header>
+            <p>Estudante de sistemas de informação no IFF campos campus centro.</p>
+            <a href="https://github.com/LucasFreitasRocha">Acessar perfil github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/33586465?v=4" alt="Lucas Rocha"/>
+              <div className="user-info">
+                <strong>Lucas Rocha</strong>
+                <span>php, ruby, js</span>
+              </div>
+            </header>
+            <p>Estudante de sistemas de informação no IFF campos campus centro.</p>
+            <a href="https://github.com/LucasFreitasRocha">Acessar perfil github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/33586465?v=4" alt="Lucas Rocha"/>
+              <div className="user-info">
+                <strong>Lucas Rocha</strong>
+                <span>php, ruby, js</span>
+              </div>
+            </header>
+            <p>Estudante de sistemas de informação no IFF campos campus centro.</p>
+            <a href="https://github.com/LucasFreitasRocha">Acessar perfil github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 }
 
